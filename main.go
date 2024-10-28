00e0f1510b5c1073eae41faf4ca19474b5ddfe76
Nahuel <nahuellitvack1122@gmail.com>
2024-10-28 12:54:18 -0300

    version 0.3

--- main.go
+++ main.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 
 	"github.com/nahuel-litvack/repositorio01/utilidades"
+
+	"time"
 )
 
 // A mejorar:
-// . Hacer que se puedan agregar espacios y no se puedan agregar numeros al ingresar el nombre de contacto.
-// . Mejorar el aspecto visual de la consola.
+// . Agregar pruebas unitarias a las funcionalidades y al diccionario.
 // . Agregar una GUI (interfaz grafica).
 // y asi terminamos el proyecto...
 
@@ -20,10 +21,13 @@ func main() {
 	for opcion == 1 || opcion == 2 || opcion == 3 || opcion == 4 || opcion == 5 {
 		// se ejecuta mientra la opcion elegida sea 1: agrega un contacto al diccionario
 		for opcion == 1 {
+			utilidades.ClearScreen()
 			utilidades.ValidacionOpcionUno()
+			utilidades.ClearScreen()
 			resultado := utilidades.AgregarContacto(utilidades.Telefono, utilidades.Nombre, utilidades.Correo)
 			fmt.Println(resultado)
-			fmt.Print("\n\n")
+			time.Sleep(2 * time.Second)
+			utilidades.ClearScreen()
 			opcion = utilidades.MenuDeInicio() // se ejecuta el menu de inicio
 		}
 
@@ -31,13 +35,18 @@ func main() {
 		for opcion == 2 {
 			// Validacion de dominio que verifica que el diccionario no este vacio
 			if utilidades.Dict.Size() == 0 {
-				fmt.Println("No hay ningun contacto en la lista.")
+				fmt.Println(string(utilidades.ColorYellow) + "No hay ningun contacto en la lista." + string(utilidades.ColorReset))
+				time.Sleep(2 * time.Second)
+				utilidades.ClearScreen()
 				opcion = utilidades.MenuDeInicio()
 			} else {
+				utilidades.ClearScreen()
 				utilidades.ValidacionOpcionDos()
+				utilidades.ClearScreen()
 				resultado := utilidades.EliminarContacto(utilidades.Telefono)
 				fmt.Println(resultado)
-				fmt.Print("\n\n")
+				time.Sleep(2 * time.Second)
+				utilidades.ClearScreen()
 				opcion = utilidades.MenuDeInicio() // se ejecuta el menu de inicio
 			}
 		}
@@ -46,13 +55,21 @@ func main() {
 		for opcion == 3 {
 			// Validacion de dominio que verifica que el diccionario no este vacio
 			if utilidades.Dict.Size() == 0 {
-				fmt.Println("No hay ningun contacto en la lista.")
+				fmt.Println(string(utilidades.ColorYellow) + "No hay ningun contacto en la lista." + string(utilidades.ColorReset))
+				time.Sleep(2 * time.Second)
+				utilidades.ClearScreen()
 				opcion = utilidades.MenuDeInicio()
 			} else {
+				utilidades.ClearScreen()
 				utilidades.ValidacionOpcionTres()
+				utilidades.ClearScreen()
 				resultado := utilidades.BuscarContacto()
+				fmt.Print((utilidades.ColorMagenta) + "Contacto buscado: " + string(utilidades.ColorReset))
 				fmt.Println(resultado)
-				fmt.Print("\n\n")
+				fmt.Print("\n")
+				fmt.Println("En 10 segundos se regresa al menu de opciones.")
+				time.Sleep(10 * time.Second)
+				utilidades.ClearScreen()
 				opcion = utilidades.MenuDeInicio() // se ejecuta el menu de inicio
 			}
 		}
@@ -61,22 +78,32 @@ func main() {
 		for opcion == 4 {
 			// Validacion de dominio que verifica que el diccionario no este vacio
 			if utilidades.Dict.Size() == 0 {
-				fmt.Println("No hay ningun contacto en la lista.")
+				fmt.Println(string(utilidades.ColorYellow) + "No hay ningun contacto en la lista." + string(utilidades.ColorReset))
+				time.Sleep(2 * time.Second)
+				utilidades.ClearScreen()
+				opcion = utilidades.MenuDeInicio()
 			} else {
-				fmt.Println("Lista de contactos:")
+				utilidades.ClearScreen()
+				fmt.Println(string(utilidades.ColorMagenta) + "Lista de contactos:" + string(utilidades.ColorReset))
+				fmt.Print("\n")
 				for _, key := range utilidades.Dict.Keys() {
 					valor := utilidades.Dict.Get(key)
 					fmt.Printf("Nombre: %s, Telefono: %d, Correo electronico: %s\n", valor[0], key, valor[1])
 				}
+				fmt.Print("\n\n")
+				fmt.Println("En 15 segundos se regresa al menu de opciones.")
+				time.Sleep(15 * time.Second)
+				utilidades.ClearScreen()
+				opcion = utilidades.MenuDeInicio()
 			}
-			fmt.Print("\n\n")
-			opcion = utilidades.MenuDeInicio() // se ejecuta el menu de inicio
 		}
 
 		// se ejecuta si la opcion elegida es 5: termina el programa
 		if opcion == 5 {
-			println("Fin del programa. Hasta luego!")
-			fmt.Print("\n")
+			utilidades.ClearScreen()
+			println(string(utilidades.ColorBlue) + "Fin del programa. Hasta luego!" + string(utilidades.ColorReset))
+			time.Sleep(3 * time.Second)
+			utilidades.ClearScreen()
 			break
 		}
 	}
--- utilidades/funcionalidades.go
+++ utilidades/funcionalidades.go
@@ -1,9 +1,14 @@
 package utilidades
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"os/exec"
 	"regexp"
+	"runtime"
 	"strconv"
+	"strings"
 )
 
 // Variables de entorno
@@ -13,39 +18,66 @@ var Telefono int
 var Nombre string
 var Correo string
 
+var ColorReset = "\033[0m"
+var ColorRed = "\033[31m"
+var ColorGreen = "\033[32m"
+var ColorYellow = "\033[33m"
+var ColorBlue = "\033[34m"
+var ColorMagenta = "\033[35m"
+var ColorCyan = "\033[36m"
+
 // ***** MENU DE OPCIONES *****
 
 // post: se despliega en consola el menu de opciones y retornara aquella que el usuario elija
 func MenuDeInicio() int {
 	// Opciones a elegir por el usuario
-	fmt.Print("\n\n")
-	fmt.Println("Menu de opciones: ")
-	fmt.Println("a. Seleccione 1 para agregar un nuevo contacto.")
-	fmt.Println("b. Seleccione 2 para eliminar un contacto.")
-	fmt.Println("c. Seleccione 3 para buscar un contacto en la lista.")
-	fmt.Println("d. Seleccione 4 para ver la lista completa de contactos.")
-	fmt.Println("e. Seleccione 5 para finalizar el programa.")
+	fmt.Println(string(ColorMagenta) + "Menu de opciones: " + string(ColorReset))
+	fmt.Println(string(ColorMagenta) + "~~~~~~~~~~~~~~~~~~~~~~~~~~~" + string(ColorReset))
+	fmt.Println("-> 1 para agregar un nuevo contacto.")
+	fmt.Println("-> 2 para eliminar un contacto.")
+	fmt.Println("-> 3 para buscar un contacto en la lista.")
+	fmt.Println("-> 4 para ver la lista completa de contactos.")
+	fmt.Println(string(ColorCyan) + "-> 5 para finalizar el programa." + string(ColorReset))
 	fmt.Print("\n\n")
 
 	// VALIDADOR DE OPCIONES: validacion de dominio que verifica que los valores ingresados sean correctos
-	fmt.Print("Seleccione una opcion (1-5): ")
+	fmt.Print("Seleccione una opcion (1 - 5): ")
 	fmt.Scanln(&opcion)
 	for opcion != 1 && opcion != 2 && opcion != 3 && opcion != 4 && opcion != 5 {
-		fmt.Print("Seleccione una opcion valida (1-5): ")
+		fmt.Print("Seleccione una opcion valida (1 - 5): ")
 		fmt.Scanln(&opcion)
 	}
 	fmt.Print("\n\n")
 	return opcion // valor de retorno
 }
 
+// post: se limpia el contenido de la consola para reiniciarla
+func ClearScreen() {
+	switch runtime.GOOS {
+	case "windows":
+		cmd := exec.Command("cmd", "/c", "cls") // Comando para Windows
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	default:
+		cmd := exec.Command("clear") // Comando para Unix
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+}
+
 // ***** VALIDACIONES DE DOMINIO *****
 
 // post: se verifica que los datos ingresados por el usuario cumplan con los requisitos
 func ValidacionOpcionUno() {
-	// Expresion regular para validar los caracteres del correo
-	re := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$`)
 
-	fmt.Println("Ingrese a continuacion el telefono, nombre y correo electronico del contacto: ")
+	// Expresion regular para validar los caracteres del correo
+	reCorreo := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$`)
+	// Expresión regular para validar el nombre
+	reNombre := regexp.MustCompile(`^[A-Za-z\s]+$`)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println(string(ColorMagenta) + "Ingrese a continuacion el telefono, nombre y correo electronico del contacto: " + string(ColorReset))
 	fmt.Print("\n")
 
 	// se valida el numero de celular del contacto a ingresar
@@ -55,46 +87,44 @@ func ValidacionOpcionUno() {
 		fmt.Print("Ingrese un telefono valido (10 digitos): ")
 		fmt.Scanln(&Telefono)
 	}
-	fmt.Print("\n")
 
 	// se valida el nombre del contacto a ingresar
-	fmt.Print("Ingrese el nombre del contacto: ")
-	fmt.Scanln(&Nombre)
-	for len(Nombre) < 2 {
+	fmt.Print("Ingrese el nombre y apellido del contacto (solo letras): ")
+	Nombre, _ = reader.ReadString('\n')
+	Nombre = strings.TrimSpace(Nombre)
+	for !reNombre.MatchString(Nombre) || len(Nombre) < 2 {
 		fmt.Print("Ingrese el nombre del contacto: ")
-		fmt.Scanln(&Nombre)
+		Nombre, _ = reader.ReadString('\n')
+		Nombre = strings.TrimSpace(Nombre)
 	}
-	fmt.Print("\n")
 
 	// se valida el correo electronico del contacto a ingresar
 	fmt.Print("Ingrese el correo del contacto: ")
 	fmt.Scanln(&Correo)
 	// mientras el correo no tenga la estructura solicitada, se volvera a pedirlo
-	for !re.MatchString(Correo) {
+	for !reCorreo.MatchString(Correo) {
 		fmt.Print("Ingrese el correo del contacto: ")
 		fmt.Scanln(&Correo)
 	}
 }
 
-// post: el usuario ingresa el nombre del contacto a eliminar de la lista
+// post: el usuario ingresa el telefono del contacto a eliminar de la lista
 func ValidacionOpcionDos() {
-	valor := Dict.Get(Telefono)
-	fmt.Print("Ingrese el nombre del contacto que desea eliminar: ")
-	fmt.Scanln(&Nombre)
-	for valor[0] != Nombre {
-		fmt.Print("El nombre ingresado no existe, ingrese el nombre del contacto que desea eliminar: ")
-		fmt.Scanln(&Nombre)
+	fmt.Print("Ingrese el telefono del contacto que desea eliminar: ")
+	fmt.Scanln(&Telefono)
+	for !Dict.Contains(Telefono) {
+		fmt.Print("El telefono ingresado no existe, ingrese el telefono del contacto que desea eliminar: ")
+		fmt.Scanln(&Telefono)
 	}
 }
 
 // post: el usuario ingresa el nombre del contacto a buscar en la lista
 func ValidacionOpcionTres() {
-	valor := Dict.Get(Telefono)
-	fmt.Print("Ingrese el nombre del contacto que desea buscar: ")
-	fmt.Scanln(&Nombre)
-	for valor[0] != Nombre {
-		fmt.Print("El nombre ingresado no existe, ingrese el nombre del contacto que desea buscar: ")
-		fmt.Scanln(&Nombre)
+	fmt.Print("Ingrese el telefono del contacto que desea buscar: ")
+	fmt.Scanln(&Telefono)
+	for !Dict.Contains(Telefono) {
+		fmt.Print("El telefono ingresado no existe, ingrese el telefono del contacto que desea buscar: ")
+		fmt.Scanln(&Telefono)
 	}
 }
 
@@ -105,21 +135,18 @@ func ValidacionOpcionTres() {
 func AgregarContacto(telefono int, nombre string, correo string) string {
 	datos := []string{nombre, correo}
 	Dict.Put(telefono, datos)
-	fmt.Print("\n")
-	return "Contacto agregado de forma exitosa!"
+	return (string(ColorGreen) + "Contacto agregado de forma exitosa!" + string(ColorReset))
 }
 
 // pre: se ingresa el numero de telefono
 // post: retornar un string diciendo que el contacto se agrego a la lista
 func EliminarContacto(telefono int) string {
 	Dict.Remove(telefono)
-	fmt.Print("\n")
-	return "Contacto eliminado"
+	return (string(ColorRed) + "Contacto eliminado." + string(ColorReset))
 }
 
 // post: retornar un string con los datos del contacto buscado por el usuario
 func BuscarContacto() string {
-	fmt.Print("\n")
 	celularStr := strconv.Itoa(Telefono)
 	return ("Nombre: " + Nombre + " Telefono: " + celularStr + " Correo: " + Correo)
 }
